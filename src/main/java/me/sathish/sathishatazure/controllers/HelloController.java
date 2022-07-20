ae8cae246f43ca2eb5e7cb3dc91540045eb2ea9e
Sathish Jayapal <sjayapal@skminfotech.com>
2022-07-19 22:36:29 -0500

    Logging filter add

--- src/main/java/me/sathish/sathishatazure/controllers/HelloController.java
+++ src/main/java/me/sathish/sathishatazure/controllers/HelloController.java
@@ -1,13 +1,19 @@
 package me.sathish.sathishatazure.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-    @RequestMapping("/")
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping
     public String index() {
+        logger.debug("Coming to index");
         return "Greetings from Azure Spring Cloud!";
     }
 }
